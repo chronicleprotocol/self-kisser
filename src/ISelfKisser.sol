65db95052db02aaf714b7ee20537e62db2f49935
Max Wickham <maxwickham@startmail.com>
2025-04-08 12:29:38 +0300

    remove supported

--- src/ISelfKisser.sol
+++ src/ISelfKisser.sol
@@ -5,23 +5,9 @@ interface ISelfKisser {
     /// @notice Thrown if SelfKisser dead.
     error Dead();
 
-    /// @notice Thrown if oracle not supported.
-    /// @param oracle The oracle not supported.
-    error OracleNotSupported(address oracle);
-
     /// @notice Emitted when SelfKisser killed.
     /// @param caller The caller's address.
     event Killed(address indexed caller);
-
-    /// @notice Emitted when support for oracle added.
-    /// @param caller The caller's address.
-    /// @param oracle The oracle that support got added.
-    event OracleSupported(address indexed caller, address indexed oracle);
-
-    /// @notice Emitted when support for oracle removed.
-    /// @param caller The caller's address.
-    /// @param oracle The oracle that support got removed.
-    event OracleUnsupported(address indexed caller, address indexed oracle);
 
     /// @notice Emitted when new address kissed on an oracle.
     /// @param caller The caller's address.
@@ -52,40 +38,11 @@ interface ISelfKisser {
 
     // -- View Functionality --
 
-    /// @notice Returns whether oracle `oracle` is supported.
-    /// @param oracle The oracle to check whether its supported.
-    /// @return True if oracle supported, false otherwise.
-    function oracles(address oracle) external view returns (bool);
-
-    /// @notice Returns the list of supported oracles.
-    ///
-    /// @dev May contain duplicates.
-    ///
-    /// @return List of supported oracles.
-    function oracles() external view returns (address[] memory);
-
     /// @notice Returns whether SelfKisser is dead.
     /// @return True if SelfKisser dead, false otherwise.
     function dead() external view returns (bool);
 
     // -- Auth'ed Functionality --
-
-    /// @notice Adds support for oracle `oracle`.
-    /// @dev Only callable by auth'ed address.
-    ///
-    /// @dev Reverts if SelfKisser not auth'ed on oracle `oracle`.
-    /// @dev Reverts if SelfKisser dead.
-    ///
-    /// @param oracle The oracle to add support for.
-    function support(address oracle) external;
-
-    /// @notice Removes support for oracle `oracle`.
-    /// @dev Only callable by auth'ed address.
-    ///
-    /// @dev Reverts if SelfKisser dead.
-    ///
-    /// @param oracle The oracle to remove support for.
-    function unsupport(address oracle) external;
 
     /// @notice Kills the contract.
     /// @dev Only callable by auth'ed address.
